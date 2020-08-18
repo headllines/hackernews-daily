61b25d224f4f90dcabe5b21fa2648676b71830f8
timqian <timqian92@qq.com>
2020-08-18 15:18:11 +0800

    change date format

--- scripts/generateHeadlinesOfDate.js
+++ scripts/generateHeadlinesOfDate.js
@@ -64,7 +64,7 @@ zx1Jq1AeM4+TceNXO9TRHHa5OC9iP08Wi451ZacHvZerJTDqwZf8DQ==
     await octokit.request('POST /repos/{owner}/{repo}/issues', {
       owner: 'headllines',
       repo: 'hackernews-daily',
-      title: `Hacker News Daily Top 10 @${date}`,
+      title: `Hacker News Daily Top 10 @${new Date().toISOString().slice(0, 10)}`,
       body: contents,
     })
   } catch (error) {
